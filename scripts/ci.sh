a1428782c7269e5b1644908cb8f1ff68d6e5d987
Tumble <tumblegamer@gmail.com>
2021-07-02 00:44:27 +0100

    -o to -P

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,7 +8,7 @@ wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2/djdev205.zip"
 wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/bnu2351b.zip"
 wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gcc930b.zip"
 wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gpp930b.zip"
-wget -p "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/mak43b.zip"
+wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/mak43b.zip"
 wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2misc/csdpmi7b.zip"
 
 sudo unzip $DJGPP_DL_LOC* -d /opt/DJGPP
