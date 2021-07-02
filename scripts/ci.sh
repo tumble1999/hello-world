d418d3f437ba35b7b96229d94c85566072ab0eb1
Tumble <tumblegamer@gmail.com>
2021-07-02 01:57:43 +0100

    auto yes

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -23,5 +23,5 @@ PATH=${DEVKITPRO}/tools/bin:$PATH
 wget -P "/tmp" "https://github.com/devkitPro/pacman/releases/download/v1.0.2/devkitpro-pacman.amd64.deb"
 #sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 sudo apt-get install gdebi-core
-sudo gdebi /tmp/devkitpro-pacman.amd64.deb
+sudo gdebi /tmp/devkitpro-pacman.amd64.deb -y
 sudo dkp-pacman -S nds-dev --noconfirm
