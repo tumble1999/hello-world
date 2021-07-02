010103ac886c0131c8c0cc1c3a0007a734f8a427
Tumble <tumblegamer@gmail.com>
2021-07-02 01:19:58 +0100

    dkp

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-sudo apt install dosbox -y
+#MSDOS 
+#sudo apt install dosbox -y
+# if [ ! -d "/opt/DJGPP"]; then 
+# mkdir -p $DJGPP_DL_LOC
+# sudo mkdir -p /opt/DJGPP
 
-if [ ! -d "/opt/DJGPP"]; then 
-mkdir -p $DJGPP_DL_LOC
-sudo mkdir -p /opt/DJGPP
+# wget -P "/tmp/DJGPP" "http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/{v2/djdev205.zip,v2gnu/bnu2351b.zip,v2gnu/gcc930b.zip,v2gnu/gpp930b.zip,v2gnu/mak43b.zip,v2misc/csdpmi7b.zip }"
 
-wget -P "/tmp/DJGPP" "http://www.mirrorservice.org/sites/ftp.delorie.com/pub/djgpp/current/{v2/djdev205.zip,v2gnu/bnu2351b.zip,v2gnu/gcc930b.zip,v2gnu/gpp930b.zip,v2gnu/mak43b.zip,v2misc/csdpmi7b.zip }"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2/djdev205.zip"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/bnu2351b.zip"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gcc930b.zip"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gpp930b.zip"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/mak43b.zip"
+# wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2misc/csdpmi7b.zip"
 
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2/djdev205.zip"
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/bnu2351b.zip"
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gcc930b.zip"
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/gpp930b.zip"
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2gnu/mak43b.zip"
-wget -P "${DJGPP_DL_LOC}" "${DJGPP_DL}/v2misc/csdpmi7b.zip"
+# sudo unzip "${DJGPP_DL_LOC}*" -d /opt/DJGPP
+#fi
 
-sudo unzip "${DJGPP_DL_LOC}*" -d /opt/DJGPP
-
-fi
+# DevKitPro
+wget -P "/tmp" "https://github.com/devkitPro/pacman/releases/download/v1.0.2/devkitpro-pacman.amd64.deb"
+sudo apt-get install gdebi-core
+sudo gdebi /tmp/devkitpro-pacman.amd64.deb
